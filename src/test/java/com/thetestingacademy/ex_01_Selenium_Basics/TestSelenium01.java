cef9093864ee80342f76392a15d7a205cf6154dd
Himaja94 <himajarayapudi@gmail.com>
2025-08-23 16:20:25 +0530

    Selenium repository

---
+++ src/test/java/com/thetestingacademy/ex_01_Selenium_Basics/TestSelenium01.java
@@ -0,0 +1,18 @@
+package com.thetestingacademy.ex_01_Selenium_Basics;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestSelenium01 {
+
+    @Test
+    public void test_Code(){
+        Assert.assertEquals("Himaja","himaja");
+    }
+    @Test
+    public void test_OpenTheTestingAcademy(){
+        FirefoxDriver driver=new FirefoxDriver();
+        driver.get("https://thetestingacademy.com");
+    }
+}
